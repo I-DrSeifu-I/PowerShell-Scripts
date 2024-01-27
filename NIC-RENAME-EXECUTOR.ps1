0d09503eba7d848aca9700ea846ed9bd4fcbf20b
I-DrSeifu-I <124513468+I-DrSeifu-I@users.noreply.github.com>
2024-01-27 18:43:20 -0500

    Update NIC-RENAME-EXECUTOR.ps1

--- NIC-RENAME-EXECUTOR.ps1
+++ NIC-RENAME-EXECUTOR.ps1
@@ -12,15 +12,8 @@ Function get-CorrectedOU($rawSub){
 
     switch($rawSub){
     
-        "Enterprise Technology Services" { return "c99494c5-e512-41dc-9197-e8255578cb6b"}
-        "Department of Radiology" { return "a7fff3ad-7cc7-4ae7-abae-4a2eb95907a1"}
-        "Researchers" { return "1dabc7b1-29be-4aa9-be13-f2a6ee39558a"}
-        "Design3D" { return "f57d0ecf-3491-403b-ba77-541e098fa2c0"}
-        "Researches-SickleCell" { return "4a357d4b-a9fc-4211-98a6-2512dcc835e4"}
-        "Graduate School" { return "cfe71554-94fb-4112-96e8-158b10d5fcb3"}
-        "HU-Football" { return "b385ec06-15a0-4403-97e6-b1275774acde"}
-        "College Of Dentistry" { return "df3d9a81-2373-485e-8d96-c698d1ceb3ff"}
-        "Citrix Infrastructure" { return "10e1799-6b32-4221-b5c4-7d5ccfbe8853"}
+        "<SUB NAME>" { return "SUB ID"}
+
         
 
     default {write-host "No case for $rawSub"}
